a8d27364825851b9f2e8883bb1715e5599cca125
Rafael Matos <matos39rafael@gmail.com>
2026-09-09 01:46:10 -0300

    feat(terraform): expose outputs and adapt EC2 for DR workflow

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -21,3 +21,14 @@ output "s3_bucket_arn" {
   description = "Bucket ARN"
   value       = aws_s3_bucket.disaster_recovery.arn
 }
+
+output "dr_backup_bucket"{
+  description = "Bucket used by litestream"
+  value = aws_s3_bucket.disaster_recovery.bucket
+}
+
+output "aws_region" {
+  description = "AWS region used by the DR environment"
+  value       = var.aws_region
+}
+
